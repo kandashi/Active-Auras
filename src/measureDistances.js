78cf41b9a96040f7844975e17e4b3524e59eb26e
Jack Holloway <jack@mrprimate.co.uk>
2022-10-25 11:29:22 +0100

    Fix up measuring debug functions

--- src/measureDistances.js
+++ src/measureDistances.js
@@ -1,4 +1,4 @@
-//bounding box check
+// bounding box check
 // center collision check
 // edges collision check
 // draw pixi polygon
@@ -6,76 +6,75 @@
 
 class AAmeasure {
 
-
     static inAura(target, source, wallblocking = false, auraHeight, radius, shape) {
-        const gs = canvas.dimensions.size
+        const gs = canvas.dimensions.size;
         const g2 = gs / 2;
 
         if (!AAmeasure.boundingCheck(target, source, radius)) return false;
-        const auraPoly = shape
+        const auraPoly = shape;
         if (AAdebug && shape) {
-            canvas.foreground.children.find(i => i.inAura)?.destroy()
-            let g = new PIXI.Graphics()
-            g.beginFill(0, 0.2).drawShape(shape)
-            let aura = canvas.foreground.addChild(g)
-            aura.inAura = true
+            canvas.layers.find((l) => l.name === "DrawingsLayer").children.find(i => i.inAura)?.destroy();
+            const g = new PIXI.Graphics();
+            g.beginFill(0, 0.2).drawShape(shape);
+            const aura = canvas.layers.find((l) => l.name === "DrawingsLayer").addChild(g);
+            aura.inAura = true;
         }
 
-        let sourceCorners = source.document.height === 1 && source.document.width === 1 ?
-            [{ x: source.center.x, y: source.center.y }] :
-            [
+        let sourceCorners = source.document.height === 1 && source.document.width === 1
+            ? [{ x: source.center.x, y: source.center.y }] 
+            : [
                 { x: source.center.x, y: source.center.y },
                 { x: source.x + g2, y: source.y + g2 },
                 { x: source.x + (source.document.width * gs) - g2, y: source.y + g2 },
                 { x: source.x + g2, y: source.y + (source.document.height * gs) - g2 },
-                { x: source.x + (source.document.width * gs) - g2, y: source.y + (source.document.height * gs) - g2 }
-            ]
+                { x: source.x + (source.document.width * gs) - g2, y: source.y + (source.document.height * gs) - g2 },
+            ];
 
-        let targetCorners = target.document.height === 1 && target.document.width === 1 ?
-            [{ x: target.center.x, y: target.center.y, collides: false }] :
-            [
+        let targetCorners = target.document.height === 1 && target.document.width === 1
+            ? [{ x: target.center.x, y: target.center.y, collides: false }]
+            : [
                 { x: target.center.x, y: target.center.y, collides: false },
                 { x: target.x + g2, y: target.y + g2, collides: false },
                 { x: target.x + (target.document.width * gs) - g2, y: target.y + g2, collides: false },
                 { x: target.x + g2, y: target.y + (target.document.height * gs) - g2, collides: false },
-                { x: target.x + (target.document.width * gs) - g2, y: target.y + (target.document.height * gs) - g2, collides: false }
-            ]
-        if (AAdebug) {
-            canvas.foreground.children.filter(i => i.squares)?.forEach(i => i.destroy())
-            function drawSquare(point) {
-                let {x, y} = point
-                let g = new PIXI.Graphics()
-                g.beginFill(0xFF0000, 0.2).drawRect(x-5, y-5, 10, 10)
-                let aura = canvas.foreground.addChild(g)
-                aura.squares = true
-            }
-            sourceCorners.forEach(i => drawSquare(i))
-            targetCorners.forEach(i => drawSquare(i))
+                { x: target.x + (target.document.width * gs) - g2, y: target.y + (target.document.height * gs) - g2, collides: false },
+            ];
 
+        if (AAdebug) {
+            canvas.layers.find((l) => l.name === "DrawingsLayer").children.filter(i => i.squares)?.forEach(i => i.destroy());
+            function drawSquare(point) {
+                const {x, y} = point;
+                const g = new PIXI.Graphics();
+                g.beginFill(0xFF0000, 0.2).drawRect(x-5, y-5, 10, 10);
+                const aura = canvas.layers.find((l) => l.name === "DrawingsLayer").addChild(g);
+                aura.squares = true;
+            }
+            sourceCorners.forEach(i => drawSquare(i));
+            targetCorners.forEach(i => drawSquare(i));
         }
-        for (let t of targetCorners) {
+
+        for (const t of targetCorners) {
             if (!auraPoly.contains(t.x, t.y)) continue; // quick exit if not in the aura
 
-            for (let s of sourceCorners) {
-                let r = new Ray(t, s)
+            for (const s of sourceCorners) {
+                const r = new Ray(t, s)
                 if (wallblocking) {
                     let collision;
                     if (game.modules.get("levels")?.active) {
-
-                        collision = _levels.testCollision({ x: t.x, y: t.y, z: target.document.elevation }, { x: s.x, y: s.y, z: source.document.elevation ?? source.flags?.levels?.elevation }, "collision")
+                        collision = _levels.testCollision({ x: t.x, y: t.y, z: target.document.elevation }, { x: s.x, y: s.y, z: source.document.elevation ?? source.flags?.levels?.elevation }, "collision");
                     }
                     else {
-                        collision = canvas.walls.checkCollision(r)
+                        collision = canvas.walls.checkCollision(r);
                     }
-                    if (collision) continue
+                    if (collision) continue;
                 }
                 if (auraHeight) {
-                    if (!AAmeasure.heightCheck(source, target, radius, r)) continue
+                    if (!AAmeasure.heightCheck(source, target, radius, r)) continue;
                 }
-                return true
+                return true;
             }
         }
-        return false
+        return false;
     }
 
     static isTokenInside(templateDetails, token, wallsBlockTargeting = false) {
@@ -134,15 +133,15 @@ class AAmeasure {
         let distance;
         switch (game.settings.get("ActiveAuras", "vertical-euclidean")) {
             case true: {
-                distance = Math.abs(source.document.elevation - target.document.elevation)
+                distance = Math.abs(source.document.elevation - target.document.elevation);
             }
                 break;
             case false: {
-                let g = canvas.dimensions
-                let a = r.distance / g.size * g.distance;
-                let b = (source.document.elevation - target.document.elevation)
-                let c = (a * a) + (b * b)
-                distance = Math.sqrt(c)
+                const g = canvas.dimensions;
+                const a = r.distance / g.size * g.distance;
+                const b = (source.document.elevation - target.document.elevation);
+                const c = (a * a) + (b * b);
+                distance = Math.sqrt(c);
             }
         }
         return distance <= radius;
@@ -156,21 +155,20 @@ class AAmeasure {
      * @returns boolean
      */
     static boundingCheck(t1, t2, radius) {
-        let { size, distance } = canvas.dimensions
-        let rad = (radius / distance) * size
-        const xMax = t2.x + rad + t2.w + (size * t1.document.width)
-        const xMin = t2.x - rad - (size * t1.document.width)
-        const yMax = t2.y + rad + t2.h + (size * t1.document.height)
-        const yMin = t2.y - rad - (size * t1.document.height)
+        const { size, distance } = canvas.dimensions;
+        const rad = (radius / distance) * size;
+        const xMax = t2.x + rad + t2.w + (size * t1.document.width);
+        const xMin = t2.x - rad - (size * t1.document.width);
+        const yMax = t2.y + rad + t2.h + (size * t1.document.height);
+        const yMin = t2.y - rad - (size * t1.document.height);
         if (AAdebug) {
-            canvas.foreground.children.find(i => i.boundingCheck)?.destroy()
-            let g = new PIXI.Graphics()
-            g.beginFill(0, 0.1).drawRect(xMin, yMin, (xMax - xMin), (yMax - yMin))
-            let check = canvas.foreground.addChild(g)
-            check.boundingCheck = true
+            canvas.layers.find((l) => l.name === "DrawingsLayer").children.find(i => i.boundingCheck)?.destroy();
+            let g = new PIXI.Graphics();
+            g.beginFill(0, 0.1).drawRect(xMin, yMin, (xMax - xMin), (yMax - yMin));
+            let check = canvas.layers.find((l) => l.name === "DrawingsLayer").addChild(g);
+            check.boundingCheck = true;
         }
         return !(t1.x < xMin || t1.x > xMax || t1.y > yMax || t1.y < yMin);
     }
-
 
 }
