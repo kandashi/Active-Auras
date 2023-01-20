c7c8284572a7f40142e2da1f161f3445ac362e1f
Jack Holloway <jack@mrprimate.co.uk>
2023-01-20 18:27:47 +0000

    remove dug statement

--- src/AAhelpers.js
+++ src/AAhelpers.js
@@ -216,9 +216,6 @@ class AAhelpers {
     }
 
     static EvaluateRollString({ rollString, token, item, deterministic=false}={}) {
-        console.warn("evaluate", {
-            rollString, token, item, deterministic
-        })
         if (Number.isInteger(Number.parseInt(`${rollString}`.trim()))) return rollString;
 
         const actor = token.actor ?? token.parent;
